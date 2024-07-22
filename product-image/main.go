815e0a97be4017bd1392e8e18e41259308fe6ff2
shahin rahimi <sh.rahimi.dev@gmail.com>
2024-07-23 00:22:21 +0330

    update

--- product-image/main.go
+++ product-image/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
+	"net/http"
 	"os"
+	"os/signal"
 	"product-image/files"
 	"product-image/handlers"
+	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/hashicorp/go-hclog"
 	"github.com/joho/godotenv"
 )
@@ -42,4 +47,54 @@ func main() {
 
 	// create the handler
 	fh := handlers.NewFiles(store, l)
+
+	// create a new serve mux
+	sm := mux.NewRouter()
+
+	// filename regex: {filename:[a-zA-Z]+\\.[a-z]{3}}
+	// problem with FileServer is that it is dumb
+	ph := sm.Methods(http.MethodPost).Subrouter()
+	ph.HandleFunc("/images/{id:[0-9]+}/{filename:[a-zA-Z]+\\.[a-z]{3}}", fh.ServeHTTP)
+
+	// get files
+	gh := sm.Methods(http.MethodGet).Subrouter()
+	gh.Handle(
+		"/images/{id:[0-9]+}/{filename:[a-zA-Z]+\\.[a-z]{3}}",
+		http.StripPrefix("/images/", http.FileServer(http.Dir(basePath))),
+	)
+
+	// create a new server
+	s := http.Server{
+		Addr:         listenAddr,        // configure the bind address
+		Handler:      sm,                // set the default handler
+		ErrorLog:     sl,                // the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
+	}
+
+	// start the server
+	go func() {
+		l.Info("Starting server", "bind_address", listenAddr)
+
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Error("Unable to start server", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	// trap sigterm or interupt and gracefully shutdown the server
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	// Block until a signal is received.
+	sig := <-c
+	l.Info("Shutting down server with", "signal", sig)
+
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
+	defer cancel()
 }
